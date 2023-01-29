76356350cf6f08b0e621fce164f37967d9575c61
Hari Prasad G <hariprasas154@gmail.com>
2023-01-29 14:56:15 +0530

    added

--- src/Com/BridgeLabs/IntListClass.java
+++ src/Com/BridgeLabs/IntListClass.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 import java.lang.reflect.Array;
 
 public class IntListClass {
-	  
 
-  Integer maxNumInt;
-  Float maxNumFloat;
-  void Max(Integer numOne, Integer numTwo, Integer numThree) {
-      if (numTwo.compareTo(maxNumInt) > 0) {
-          System.out.println(numTwo + " Num Two is greater");
-      } else if (numThree.compareTo(maxNumInt) > 0) {
-          System.out.println(numThree + " Num Three is greater");
-      } else {
-          System.out.println(maxNumInt + " Num One is greater");
-      }
-  }
-  
- 
- 
+
+	Integer maxNumInt;
+	Float maxNumFloat;
+	void Max(Integer numOne, Integer numTwo, Integer numThree) {
+		if (numTwo.compareTo(maxNumInt) > 0) {
+			System.out.println(numTwo + " Num Two is greater");
+		} else if (numThree.compareTo(maxNumInt) > 0) {
+			System.out.println(numThree + " Num Three is greater");
+		} else {
+			System.out.println(maxNumInt + " Num One is greater");
+		}
+	}
+	void Max(Float numOne, Float numTwo, Float numThree) {
+
+		if (numTwo.compareTo(maxNumFloat) > 0) {
+			System.out.println(numTwo + " Num Two is greater");
+		} else if (numThree.compareTo(maxNumFloat) > 0) {
+			System.out.println(numThree + " Num Three is greater");
+		} else {
+			System.out.println(maxNumFloat + " Num One is greater");
+		}
+	}
+
+
+
+
+
 }
+
 
 
--- src/Com/BridgeLabs/Main.java
+++ src/Com/BridgeLabs/Main.java
@@ -2,14 +2,14 @@ package Com.BridgeLabs;
 
 public class Main {
 
-	
-		public static void main(String[] args) {
-			IntListClass Obj = new IntListClass();
-      Obj.Max(2, 1, 4);
-      
-			
-		}
+
+	public static void main(String[] args) {
+		IntListClass Obj = new IntListClass();
+		Obj.Max(2, 1, 4);
+		Obj.Max(2.2f, 10.2f, 4.2f);
 
 	}
+
+}
 
 
